5f503f048b9325f9188cdc68e14a2058701b603f
anshulbagal <anshulbagal5@gmail.com>
2026-03-29 13:04:50 +0530

    Time: 0 ms (100%), Space: 8.2 MB (67.1%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        if (s1.size() != s2.size()) return false;
-
-        // group 1: index 0 and 2
-        string a = {s1[0], s1[2]};
-        string b = {s2[0], s2[2]};
-
-        // group 2: index 1 and 3
-        string c = {s1[1], s1[3]};
-        string d = {s2[1], s2[3]};
-
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-        sort(c.begin(), c.end());
-        sort(d.begin(), d.end());
-
-        return (a == b) && (c == d);
+        return 
+            ( (s1[0]==s2[0] && s1[2]==s2[2]) || (s1[0]==s2[2] && s1[2]==s2[0]) )
+        &&
+            ( (s1[1]==s2[1] && s1[3]==s2[3]) || (s1[1]==s2[3] && s1[3]==s2[1]) );
     }
 };
