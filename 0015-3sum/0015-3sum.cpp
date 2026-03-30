2f340b253a73e7d74a5fdade6220c76a8a056e2a
anshulbagal <anshulbagal5@gmail.com>
2026-03-31 00:01:19 +0530

    Time: 40 ms (90.49%), Space: 29.1 MB (45.48%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,8 +6,10 @@ public:
 
         for (int i = 0; i < nums.size(); i++) {
 
+            // optimization: agar current number > 0 hai to aage sab positive honge
             if (nums[i] > 0) break;
 
+            // skip duplicates for i
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int l = i + 1;
@@ -16,22 +18,26 @@ public:
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
 
-                if (sum > 0) {
-                    r--;
-                } 
-                else if (sum < 0) {
+                if (sum < 0) {
                     l++;
+                } 
+                else if (sum > 0) {
+                    r--;
                 } 
                 else {
                     res.push_back({nums[i], nums[l], nums[r]});
                     l++;
                     r--;
 
+                    // skip duplicates for l
                     while (l < r && nums[l] == nums[l - 1]) l++;
+
+                    // skip duplicates for r
                     while (l < r && nums[r] == nums[r + 1]) r--;
                 }
             }
         }
+
         return res;
     }
 };
